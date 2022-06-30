5f76fedef9760d6f842fb2a34b586419b7a9f100
bot5294 <5294aqib@gmail.com>
2022-06-30 15:20:16 +0530

    Added mannual sign-in

---
+++ config/mongoose.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/codeial_development');
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+
+db.once('open', function(){
+    console.log('Connected to Database :: MongoDB');
+});
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+module.exports = db;
--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
 
-const db = require('mongoose');
+app.use(express.urlencoded({extended:true}));
 
-app.use(express.json());
+app.use(cookieParser());
+
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 
 // use express router
-app.use('/',require('./routes'));
+app.use('/', require('./routes'));
 
 // set up the view engine
-app.use('view engine','ejs')
-app.set('views','./views');
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
-app.listen(port,function(err){
-    if(err){
-        console.log(`Error in starting the server: ${err}`)
+
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
     }
-    console.log(`Server up and running at port: http://127.0.0.1:${port}`);
-})
+
+    console.log(`Server is running on port: ${port}`);
+});
