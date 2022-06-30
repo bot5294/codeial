29bf40b7ffcbdeb79521b8aa507389cf1b27a7c6
bot5294 <5294aqib@gmail.com>
2022-06-30 23:36:14 +0530

    mannual authentication complete

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,7 @@ router.get('/sign-in', usersController.signIn);
 
 
 router.post('/create', usersController.create);
-
+router.post('/create-session',usersController.createSession);
+router.get('/logout',usersController.logout)
 
 module.exports = router;
