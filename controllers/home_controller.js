061407640eab099c40459fe6130ddcb2407ffb80
bot5294 <5294aqib@gmail.com>
2022-08-06 16:24:09 +0530

    converted to async code

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,32 +1,36 @@
 const Post = require('../models/post');
-
-module.exports.home = function(req, res){
-    // console.log(req.cookies);
-    // res.cookie('user_id', 25);
-
-    // Post.find({}, function(err, posts){
-    //     return res.render('home', {
-    //         title: "Codeial | Home",
-    //         posts:  posts
-    //     });
-    // });
-
-    // populate the user of each post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
+const User = require('../models/user');
+module.exports.home = async function(req, res){
+    try{
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        })
+    
+        let users = await User.find({});
+    
         return res.render('home', {
             title: "Codeial | Home",
-            posts:  posts
-        });
-    })
-
+            posts:  posts,
+            all_users:users
+        })
+    
+    }catch(err){
+        console.log('Error',err);
+        return;
+    }
+    
 }
 
 // module.exports.actionName = function(req, res){}
+
+// using then
+// Post.find({}).populate('comments').then(function());
+
+// let posts = Post.find({}).populate('comments').exec();
+// posts.then()
+
